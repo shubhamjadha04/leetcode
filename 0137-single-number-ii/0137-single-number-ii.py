aaf501db6a321c57af884d754f070a929f9e1df4
Shubham Jadhao <shubhamjadhao1124@gmail.com>
2026-09-03 12:58:51 +0530

    Time: 0 ms (100%), Space: 20.9 MB (29.54%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = {}
+
+        for key in nums:
+            if key in freq:
+                freq[key]+=1
+            
+            else:
+                freq[key] = 1
+
+        
+        for key in freq:
+            if freq[key] == 1:
+                return key
+        
